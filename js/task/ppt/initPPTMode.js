cf82468a25a45b50e9df60dde0270ac2ff9410c8
kirakiray <pikay@foxmail.com>
2019-06-12 22:10:23 +0800

    backup

--- js/task/ppt/initPPTMode.js
+++ js/task/ppt/initPPTMode.js
@@ -14,11 +14,39 @@ drill.task(async (load, data) => {
     let pageFirst = mainEle.children().eq(initActiveId);
     let pageData = pageFirst.prop("pageData");
 
+    commonData.on("pageLoaded", (e, d) => {
+        // 清空内元素进行动画
+        pageUtil.clearPageAnime(d.page);
+
+        console.log(d);
+    });
+
     // 点火第一个
     await pageData.startLoad();
 
-    // commonData.on("pageLoaded", (e, d) => {
-    //     // 清空内元素进行动画
-    //     pageUtil.clearPageAnime(d.page);
-    // });
+    // 添加相应的active
+    $(".p_main > .page").each((pageId, e) => {
+        let $page = $(e);
+
+        if (initActiveId === pageId) {
+            $page.attr("active", 1);
+        } else {
+            $page.attr("active", 0);
+        }
+
+        let pageData = e.pageData;
+
+        // 提前设置样式
+        if (pageId == initActiveId) {
+            // 清空transform
+            $page.css(animationCSSUtil.toCSSObj(pageData.pos1_after));
+        } else if (pageId < initActiveId) {
+            $page.css(animationCSSUtil.toCSSObj(pageData.pos2_after));
+        } else {
+            $page.css(animationCSSUtil.toCSSObj(pageData.pos1));
+        }
+    });
+
+
+    pageUtil.runPageAnime(pageFirst);
 });
